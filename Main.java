2f1ecbbea04a0fb00d048e9dd77b5e24366546a1
SnihMV <41325727+SnihMV@users.noreply.github.com>
2018-09-18 15:42:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -32,7 +32,6 @@ public class Main {
     private static <T> void printArray(T[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        
     }
 }
